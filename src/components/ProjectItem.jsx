6709b09a0850dc0019f6a9dc73271be9b76d945b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-24 16:26:57 -0700

    adding some styling to porject item component

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const ProjectItem = ({ img, title }) => {
   return (
-    <div>
-      <img src={img} alt="" />
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group">
+      <img
+        src={img}
+        alt="images"
+        className="rounded-xl group-hover:opacity-10"
+      />
+      <div>
+        <h3>{title}</h3>
+      </div>
     </div>
   );
 };
