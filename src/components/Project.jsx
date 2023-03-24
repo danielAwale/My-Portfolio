e62150b1e0ef4666db8f10fe1509b8a8a002944a
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-24 14:21:45 -0700

    contain imports

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import ProjectItem from "./ProjectItem";
+import Img1 from "../assets/project-img1.png";
+import Img2 from "../assets/project-img2.png";
+import Img3 from "../assets/project-img3.png";
+import Img4 from "../assets/project-img4.png";
 
 const Project = () => {
   return <div id="project">Project</div>;
