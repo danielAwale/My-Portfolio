909f3b10963283ffe0f86a2fb7d96cd3a938cf52
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-24 14:39:57 -0700

    begin work on the projects componenet

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,17 @@ import Img3 from "../assets/project-img3.png";
 import Img4 from "../assets/project-img4.png";
 
 const Project = () => {
-  return <div id="project">Project</div>;
+  return (
+    <div id="project" className="section flex flex-col">
+      <h1 className="text-4xl font-bold text-center">Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quod,
+        ut in vel unde tempore, sequi laboriosam quisquam dolore mollitia
+        eveniet iusto voluptate impedit officiis repudiandae obcaecati sint
+        omnis tempora.
+      </p>
+    </div>
+  );
 };
 
 export default Project;
