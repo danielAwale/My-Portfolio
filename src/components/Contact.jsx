41ff742c2489cc2d840e7d176e5fabefeb37c4d7
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-23 17:10:41 -0700

    work on the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Contact = () => {
   return (
     <section id="contact" className="section">
-      <div className="container mx-auto"></div>
+      <div className="container mx-auto">
+        <div className="flex flex-row">
+          {/* text */}
+          <div className="flex-1">
+            <div>
+              <h4>Let's get in touch!</h4>
+            </div>
+          </div>
+          {/* form */}
+          <div className="flex-1">form</div>
+        </div>
+      </div>
     </section>
   );
 };
