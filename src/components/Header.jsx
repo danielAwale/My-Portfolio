2f5523ae1d71650d3227b17b2df5b28b4cd27bbc
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-28 13:51:31 -0700

    Contain my resume instead of resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             rel="noreferrer"
           >
             <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded">
-              Resume
+              My Resume
             </button>
           </a>
         </div>
